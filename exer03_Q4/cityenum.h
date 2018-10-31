967985d8fd5c14c6f92eab3e3d9b17fd24144bd2
18wang <jswxingyu@outlook.com>
2018-10-31 11:45:15 +0800

    实现日期多选框功能

---
+++ exer03_Q4/cityenum.h
@@ -0,0 +1,15 @@
+#ifndef CITYENUM_H
+#define CITYENUM_H
+
+ namespace CITY {
+ enum city {
+    "南京" = "nanjing",
+    "北京" = "beijing",
+    "上海" = "shanghai",
+     "杭州" ="hangzhou",
+     "哈尔滨" = "haerbin",
+     "苏州" = "suzhou"
+ };
+ }
+
+#endif // CITYENUM_H
